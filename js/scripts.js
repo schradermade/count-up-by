8b7eeab640b37d33938961a940982628f659f3bd
Nathan Schrader <nathanschrader@icloud.com>
2020-10-27 13:21:57 -0700

    add proper functionality to for-loop
    Co-authored-by: Diego Salazar <salazardiego343@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $("form#counting").submit(function(event) {
     event.preventDefault();
-    let countBy = parseInt($("#input1").val());
-    let countToNumber = parseInt($("#input2").val());
-    let countToNumber = parseInt($("#input2").val());
 
-  for (let i = 0; i <= countToNumber; i += countBy) {
+    let startNumber;
+    let countToNumber = parseInt($("#input1").val());
+    let countBy = parseInt($("#input2").val());
+    //let countToNumber = parseInt($("#input2").val());
+
+  for (let i = 0 ; i <= countToNumber; i += countBy) {
+    
     console.log(i)
     }
   });
